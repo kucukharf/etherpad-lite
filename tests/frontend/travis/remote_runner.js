4bc2c4070fc16da0e5c5afaba9bc320efd5ad8bf
webzwo0i <webzwo0i@c3d2.de>
2020-10-22 01:05:39 +0200

    test

--- tests/frontend/travis/remote_runner.js
+++ tests/frontend/travis/remote_runner.js
@@ -4,36 +4,33 @@ const { Builder }= require(srcFolder + "selenium-webdriver");
 const baseUrl = "http://localhost:9001/tests/frontend";
 
 let driver;
-
-(async function(){
-driver = await new Builder().withCapabilities({
-            'browserName': 'chrome',
-            'platformName': 'Windows 10',
-            'browserVersion': 'latest',
-            'goog:chromeOptions' : { 'w3c' : true },
-            'sauce:options': {
-                'username': process.env.SAUCE_USERNAME,
-                'accessKey': process.env.SAUCE_ACCESS_KEY,
-                'build': process.env.GIT_HASH,
-                'extendedDebugging': true,
-                'tunnelIdentifier': process.env.TRAVIS_JOB_NUMBER,
-                'name': 'test',
-                /* As a best practice, set important test metadata and execution options
-                such as build info, tags for reporting, and timeout durations.
-                */
-                'maxDuration': 600,
-                'idleTimeout': 1000
-            }
-        }).usingServer("https://ondemand.saucelabs.com/wd/hub").build();
-})()
-
 let knownConsoleText;
 let getStatusInterval;
 let timeout;
+let allTestsPassed;
 
 runTest()
 
 async function runTest(){
+  driver = await new Builder().withCapabilities({
+    'browserName': 'chrome',
+    'platformName': 'Windows 10',
+    'browserVersion': 'latest',
+    'goog:chromeOptions' : { 'w3c' : true },
+    'sauce:options': {
+        'username': process.env.SAUCE_USERNAME,
+        'accessKey': process.env.SAUCE_ACCESS_KEY,
+        'build': process.env.GIT_HASH,
+        'extendedDebugging': true,
+        'tunnelIdentifier': process.env.TRAVIS_JOB_NUMBER,
+        'name': 'test',
+        /* As a best practice, set important test metadata and execution options
+        such as build info, tags for reporting, and timeout durations.
+        */
+        'maxDuration': 600,
+        'idleTimeout': 1000
+    }
+  }).usingServer("https://ondemand.saucelabs.com/wd/hub").build();
   await driver.getSession().then(function (sessionid) {
     console.log(`https://saucelabs.com/jobs/${sessionid.id_}`);
     return driver.sessionID = sessionid.id_;
@@ -72,7 +69,6 @@ async function runTest(){
   });
 }
 
-let allTestsPassed;
 //tear down the test excecution
 async function stopSauce(success,timesup){
   let testSettings = {"browserName":"chrome", "platform":"Windows 10"}
