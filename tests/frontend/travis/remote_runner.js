4c64492ab8f2a7e9290aa86181155eba336ac470
webzwo0i <webzwo0i@c3d2.de>
2020-10-21 23:37:06 +0200

    test

--- tests/frontend/travis/remote_runner.js
+++ tests/frontend/travis/remote_runner.js
@@ -1,8 +1,12 @@
-var srcFolder = "../../../src/node_modules/";
+const srcFolder = "../../../src/node_modules/";
 const { Builder }= require(srcFolder + "selenium-webdriver");
 
+const baseUrl = "http://localhost:9001/tests/frontend";
+
+let driver;
+
 (async function(){
-let driver = await new Builder().withCapabilities({
+driver = await new Builder().withCapabilities({
             'browserName': 'chrome',
             'platformName': 'Windows 10',
             'browserVersion': 'latest',
@@ -17,17 +21,82 @@ let driver = await new Builder().withCapabilities({
                 /* As a best practice, set important test metadata and execution options
                 such as build info, tags for reporting, and timeout durations.
                 */
-                'maxDuration': 100,
+                'maxDuration': 600,
                 'idleTimeout': 1000
             }
         }).usingServer("https://ondemand.saucelabs.com/wd/hub").build();
-
-await driver.getSession().then(function (sessionid) {
-  driver.sessionID = sessionid.id_;
-  console.log(driver.sessionID);
-});
-let baseUrl = "http://localhost:9001/tests/frontend";
-await driver.get(baseUrl);
-await driver.sleep(100000);
-
 })()
+
+let knownConsoleText;
+let getStatusInterval;
+let timeout;
+
+runTest()
+
+async function runTest(){
+  await driver.getSession().then(function (sessionid) {
+    console.log(`https://saucelabs.com/jobs/${sessionid.id_}`);
+    return driver.sessionID = sessionid.id_;
+  });
+
+  driver.get(baseUrl).then(function(){
+    getStatusInterval = setInterval(function(){
+      driver.eval("$('#console').text()", function(err, consoleText){
+        if(!consoleText || err){
+          return;
+        }
+        knownConsoleText = consoleText;
+
+        if(knownConsoleText.indexOf("FINISHED") > 0){
+          let match = knownConsoleText.match(/FINISHED.*([0-9]+) tests passed, ([0-9]+) tests failed/);
+          // finished without failures
+          if (match[2] && match[2] == '0'){
+            stopSauce(true);
+
+          // finished but some tests did not return or some tests failed
+          } else {
+            stopSauce(false);
+          }
+        }
+      });
+    }, 5000);
+
+    /**
+     * timeout if a test hangs or the job exceeds 9.5 minutes
+     * It's necessary because if travis kills the saucelabs session due to inactivity, we don't get any output
+     * @todo this should be configured in testSettings, see https://wiki.saucelabs.com/display/DOCS/Test+Configuration+Options#TestConfigurationOptions-Timeouts
+     */
+    timeout = setTimeout(function(){
+      stopSauce(false,true);
+    }, 570000); // travis timeout is 10 minutes, set this to a slightly lower value
+  });
+}
+
+let allTestsPassed;
+//tear down the test excecution
+async function stopSauce(success,timesup){
+  let testSettings = {"browserName":"chrome", "platform":"Windows 10"}
+  clearInterval(getStatusInterval);
+  clearTimeout(timeout);
+
+  if(!success){
+    allTestsPassed = false;
+  }
+
+  // if stopSauce is called via timeout (in contrast to via getStatusInterval) than the log of up to the last
+  // five seconds may not be available here. It's an error anyway, so don't care about it.
+  var testResult = knownConsoleText.replace(/\[red\]/g,'\x1B[31m').replace(/\[yellow\]/g,'\x1B[33m')
+                   .replace(/\[green\]/g,'\x1B[32m').replace(/\[clear\]/g, '\x1B[39m');
+  testResult = testResult.split("\\n").map(function(line){
+    return "[" + testSettings.browserName + " " + testSettings.platform + (testSettings.version === "" ? '' : (" " + testSettings.version)) + "] " + line;
+  }).join("\n");
+
+  console.log(testResult);
+  if (timesup) {
+    console.log("[" + testSettings.browserName + " " + testSettings.platform + (testSettings.version === "" ? '' : (" " + testSettings.version)) + "] \x1B[31mFAILED\x1B[39m allowed test duration exceeded");
+  }
+  console.log(`Remote sauce test ${name} finished! https://saucelabs.com/jobs/${driver.sessionID}`);
+
+  await driver.quit();
+  process.exit(allTestsPassed ? 0 : 1);
+}
